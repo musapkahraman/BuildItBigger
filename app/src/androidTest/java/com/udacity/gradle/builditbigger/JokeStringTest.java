811c1c11eabe9dc0a3403a9010957a0635e51eda
musap86 <musap86@gmail.com>
2018-05-27 08:57:05 +0300

    feat: Test AsyncTask successfully retrieves a non-empty string

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeStringTest.java
@@ -0,0 +1,36 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.jokedisplayer.JokeDisplayActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class JokeStringTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityTestRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickButton_CheckJokeTextForNonEmptyString() {
+        onView(withId(R.id.button_show_joke)).perform(click());
+        intended(hasComponent(hasClassName(JokeDisplayActivity.class.getName())));
+        onView(withId(R.id.tv_joke)).check(matches(not(withText(""))));
+    }
+}
